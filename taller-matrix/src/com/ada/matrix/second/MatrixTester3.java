e9da989a47f96c114563951bce01117d073a2d27
DavidPiracoca <you@example.com>
2018-05-28 17:51:08 -0500

    sdasd

---
+++ taller-matrix/src/com/ada/matrix/second/MatrixTester3.java
@@ -0,0 +1,31 @@
+/*
+ * MIT License 
+ * 
+ * Copyright (c) 2018 Ownk
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ */
+
+package com.ada.matrix.second;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * <h1>MatrixTester3</h1>
+ *
+ * Description
+ *
+ * @author DavidPiracoca (Ownk) 
+ * @version 1.0
+ * 
+ */
+public class MatrixTester3 {
+
+
+
+}
---
+++ taller-matrix/src/com/ada/matrix/second/MatrixTester6.java
@@ -0,0 +1,29 @@
+/*
+ * MIT License 
+ * 
+ * Copyright (c) 2018 Ownk
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ */
+
+package com.ada.matrix.second;
+
+/**
+ *
+ * <h1>MatrixTester6</h1>
+ *
+ * Description
+ *
+ * @author DavidPiracoca (Ownk) 
+ * @version 1.0
+ * 
+ */
+public class MatrixTester6 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
